c5fc27a1850ee821c1e0797ae94ac45161b54c96
Patrik <patriknil90@gmail.com>
2019-10-06 21:14:31 +0200

    Add MovieItem component

---
+++ src/components/MovieItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MovieItem = ({ movie }) => {
+  return (
+    <li key={movie.id} className="MovieItem">
+      <span className="MovieItem_episode">
+        Episode {movie.fields.episode_id}
+      </span>
+      <span className="MovieItem_title">{movie.fields.title}</span>
+      <span className="MovieItem_year">{movie.fields.created}</span>
+    </li>
+  )
+}
+
+export default MovieItem
---
+++ src/components/MovieList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import MovieItem from 'components/MovieItem'
+
+const movieList = ({ movies, loading, error }) => (
+  <section>
+    {movies && !loading && !error && (
+      <ul>
+        {movies.map(movie => (
+          <MovieItem key={movie.id} movie={movie} />
+        ))}
+      </ul>
+    )}
+    {!loading && error && (
+      <div>Error while fetching movies, please reload page</div>
+    )}
+    {loading && <div>Loading...</div>}
+  </section>
+)
+
+const mapStateToProps = state => ({
+  movies: state.movies.movies,
+  loading: state.movies.loading,
+  error: state.movies.error,
+})
+
+export default connect(mapStateToProps)(movieList)
