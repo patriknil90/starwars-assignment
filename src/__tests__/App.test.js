473e972319c8fcbe1cb7c487da84f0dcf1df7f8a
Patrik <patriknil90@gmail.com>
2019-10-06 14:21:51 +0200

    Add movie list test with mocked store

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import moviesMock from 'movieMocks'
+import Root from 'root'
 import App from '../App'
 
 describe('<App />', () => {
-  it('displays heading', () => {
-    const wrapper = shallow(<App />)
-    expect(
-      wrapper
-        .find('h1')
-        .render()
-        .text()
-    ).toBe('Minimal React Starter Kit')
+  it('displays a list of movies', () => {
+    const wrapper = mount(
+      <Root>
+        <App />
+      </Root>
+    )
+    const movieList = wrapper.find('ul')
+    expect(movieList.find('li')).toHaveLength(moviesMock.length)
+
+    movieList.find('li').forEach((listItem, index) => {
+      expect(
+        listItem
+          .find('.MovieItem_title')
+          .render()
+          .text()
+      ).toBe(moviesMock[index].fields.title)
+    })
+
+    wrapper.unmount()
   })
 })
