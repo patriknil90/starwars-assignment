a6f3adf99051ea6238defb451416e45a9b764c97
Patrik <patriknil90@gmail.com>
2019-10-07 20:56:36 +0200

    change in Filter_input triggers filterMovies action creator

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { filterMovies } from 'actions/movies'
 
-const Filter = () => {
+const Filter = ({ filterMovies }) => {
+  const onFilterChange = e => {
+    const { value } = e.target
+    filterMovies(value)
+  }
+
   return (
     <div id="Filter">
-      <input id="Filter_input" type="text" />
+      <input onChange={onFilterChange} id="Filter_input" type="text" />
     </div>
   )
 }
 
-export default Filter
+const mapDispatchToProps = dispatch => ({
+  filterMovies: filterValue => dispatch(filterMovies(filterValue)),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Filter)
